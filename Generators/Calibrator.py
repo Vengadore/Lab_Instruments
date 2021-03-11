dbb9a760e2314de3cc78c11c0d43acb0dff87112
Vengadore <Vengadore@users.noreply.github.com>
2021-03-11 10:26:54 -0600

    FLLUKE 5500A and 3458A added

---
+++ Generators/Calibrator.py
@@ -0,0 +1,36 @@
+class CAL_5500A:
+    def __init__(self, address=0):
+        rm = pyvisa.ResourceManager()
+        ## Set
+        self.instrument = rm.open_resource(rm.list_resources()[address], read_termination='\n')
+        self.GPIB_Address = rm.list_resources()[address]
+        self.IDN = self.instrument.query('*IDN?')
+        print(self.IDN)
+        self.instrument.timeout = 40000
+        self.write("RESET")
+
+    def write(self, data):
+        self.instrument.write(data)
+
+    def read(self):
+        return self.instrument.read()
+
+    def query(self, data):
+        return self.instrument.query(data)
+
+    def WAIT(self):
+        self.write(";*WAI")
+
+    def SET_output(self, amplitude, unit="V", frequency=0):
+        self.write("OUT " + str(amplitude) + " " + str(unit))
+        self.WAIT()
+        self.write("OUT " + str(frequency) + " HZ")
+        self.WAIT()
+
+    def OPERATE(self):
+        self.WAIT()
+        self.write("OPER")
+
+    def STBY(self):
+        self.WAIT()
+        self.write("STBY")
---
+++ MeasuringDevices/DMM.py
@@ -0,0 +1,63 @@
+import pyvisa
+
+
+class DMM_3458A:
+    def __init__(self, address=0):
+        rm = pyvisa.ResourceManager()
+        ## Set
+        self.instrument = rm.open_resource(rm.list_resources()[address], read_termination='\r')
+        self.GPIB_Address = rm.list_resources()[address]
+        self.IDN = self.instrument.query('ID?')
+        print(self.IDN)
+        self.instrument.timeout = 40000
+        self.write("RESET")
+
+    def write(self, data):
+        self.instrument.write(data)
+
+    def read(self):
+        return self.instrument.read()
+
+    def query(self, data):
+        return self.instrument.query(data)
+
+    def ACAL(self, function="ALL"):
+        cal_values = ["ALL", "DCV", "AC", "OHMS"]
+        if function in cal_values:
+            try:
+                self.write("ACAL " + function)
+            except:
+                temp = None
+                while (temp != None):
+                    try:
+                        temp = self.get_temp()
+                    except:
+                        temp = None
+            return 1
+        else:
+            return 0
+
+    def get_temp(self):
+        return self.query("TEMP?")
+
+    def reset(self):
+        self.write("RESET")
+
+    def set_ACV(self, range=1):
+        self.write("ACV " + str(range) + "0.00001")
+
+    def set_ACVSYNC(self, range=1):
+        self.write("ACV " + str(range) + ";")
+        self.write("SETACV SYNC;")
+        self.write("NPLC 100;NDIG 8;LFILTER ON;RES 0.000001")
+
+    def set_DCV(self, range=1, NPLC=200):
+        self.write("DCV " + str(range) + ";")
+        self.set_NPLC(NPLC)
+
+    def set_NPLC(self, NPLC=1):
+        self.write("NPLC " + str(NPLC))
+
+    def SAMPLE(self):
+        self.write("TRIG SGL")
+        return float(self.read())
