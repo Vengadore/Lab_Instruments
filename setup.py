a95a6d87e51cd064b911ee2df2caabd668456a9a
crcg1995@hotmail.com <123456789Ab>
2021-04-27 16:20:49 -0500

    Added basic Units.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Lab_Instruments',
+    version='0.1',
+    packages=['Waves', 'Generators', 'Fundamentals', 'MeasuringDevices'],
+    url='',
+    license='None',
+    author='César Rolando Carrillo Gómez',
+    author_email='crcg1995@hotmail.com',
+    description='A set of tools for working in the lab'
+)
