9a4442271279f8f8be1119f385c12a357e3a7f8a
Vengadore <Vengadore@users.noreply.github.com>
2021-06-11 10:44:06 -0500

    Update DMM.py

--- MetrologyLab/MeasuringDevices/DMM.py
+++ MetrologyLab/MeasuringDevices/DMM.py
@@ -60,3 +60,34 @@ class DMM_3458A:
     def SAMPLE(self):
         self.write("TRIG SGL")
         return float(self.read())
+    
+class DMM_34401A:
+    def __init__(self,address = 0):
+        self.inst = rm.open_resource(pv.ResourceManager().list_resources()[address])
+        self.inst.timeout = 250000
+        self.inst.write('*RST')
+        print(self.inst.query('*IDN?'))
+    def write(self,string):
+        self.inst.write(string)
+    def read(self):
+        self.inst.read()
+    def query(self,string):
+        return self.inst.query(string)
+    def AC_config(self,R = 1,Res = 0.00001):
+        self.write('CONF:VOLT:AC {}, {}'.format(R,Res))
+        self.write('SAMPle:COUNt 10')
+        self.write('CALCulate:FUNCtion AVERage')
+        self.write('CALCulate:STATe ON')
+        self.write('DET:BAND 3')
+    def Get_funciton(self):
+        return self.query('FUNCtion?')
+    def Trigger(self):
+        self.write('CALCulate:STATe OFF')
+        self.write('CALCulate:STATe ON')
+        self.write('INITiate')
+    def Measure_AC(self):
+        self.Trigger()
+        self.query('*OPC?')
+        R = self.query('READ?').split(',')
+        R = [float(f) for f in R[0:-1]] + [float(R[-1].split('\\')[0])] 
+        return R
