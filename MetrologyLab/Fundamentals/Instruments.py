4b9ddf26449d887e70c0d08b59c1c076c5ba8465
crcg1995@hotmail.com <123456789Ab>
2021-07-20 18:22:48 -0500

    Foundations of calibrator

--- MetrologyLab/Fundamentals/Instruments.py
+++ MetrologyLab/Fundamentals/Instruments.py
@@ -1,12 +1,65 @@
 import xml.etree.ElementTree as ET
+import metrolopy as uc
+
 
 class Instrument:
-    def __init__(self,path:str):
+    def __init__(self, path: str):
         self.ORIGIN = path
         self.ROOT = ET.parse(self.ORIGIN)
         print(self.ROOT.find('INFORMATION').find("MFR").text)
 
 
+class InstFunciton:
+    """An instrument function describes the capability of any instrument.
+    It describes the capability to measure or generate a certain value or quantity.
+    Inside of it certain ranges can be found."""
+
+    def __init__(self, unit: uc.unit('V'), ranges: list, mode: str = "G"):
+        """
+        :param unit: object
+        :param ranges: list
+        :param mode: Generate/Measure
+        """
+        self.unit = unit
+        self.RANGES = ranges
+        self.MODE = "Generator" if mode == "G" else "Meter"
+
+
+
+class Interval:
+    """ An Interval is the fundamental configuration of any equipment.
+    An equipment has always a nominal value that can be corrected given a calibration certificate,
+    they tend to have a minimum and maximum value of operation. A function can map the nominal value
+    to a real value with a certain uncertainty."""
+
+    def function(self):
+        return x
+
+    def __init__(self,max,min = 0,nominal:function = lambda x:uc.gummy(x),error:function = lambda x:0,**kwargs):
+        self.NOMINAL = nominal
+        self.E = error
+        self.min = min
+        self.max = max
+        self.unit = self.NOMINAL(self.min).unit
+
+
+        ## Uncertainties from different sources
+
+
+
+    def __call__(self,x):
+        return self.NOMINAL(x)-self.E(x)
+
+
+
 
 if __name__ == "__main__":
-    I = Instrument("Example.xml")
+    I = Interval(max = 0.3299999,
+                 min = 0,
+                 nominal= lambda x:(uc.gummy(x,unit = 'mV',u = 15,uunit='ppm')),
+                 error = lambda x:uc.gummy(0.0,unit = 'mV',u= 0,uunit='ppm'))
+    I.style = "+-"
+
+    print(I(100))
+
+
--- MetrologyLab/Generators/Calibrator.py
+++ MetrologyLab/Generators/Calibrator.py
@@ -1,4 +1,5 @@
 import pyvisa
+import metrolopy as uc
 
 class CAL_5500A:
     def __init__(self, address=0):
@@ -36,3 +37,32 @@ class CAL_5500A:
     def STBY(self):
         self.WAIT()
         self.write("STBY")
+
+class MFC:
+    """MFC: Multi Function Calibrator
+    This class describes the body of a common multi-function calibrator by FLUKE.
+    These calibrators are capable of working with magnitudes such as:
+    - Voltage DC
+    - Voltage AC
+    - Current DC
+    - Current AC
+    - Resistance
+    - Thermocouples
+    - Capacitance
+    """
+    def __init__(self,MODEL:str):
+        self.MODEL = MODEL
+        self.MFR = "FLUKE"
+
+    class mode:
+        """mode defines a capability of the instrument"""
+        def __init__(self,unit:uc.unit, max:float,min:float = 0,correction:lambda = lambda x = 0):
+            self.unit = unit
+            self.MAX = max
+            self.MIN = min
+            self.error = correction
+            self.uncertanty =
+        def f(self,x):
+            return x + self.error(x)
+
+
