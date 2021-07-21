3958bc82dcce7621cf4baa3d5ef6848b55ff91ea
crcg1995@hotmail.com <ElAlmaFeliz2>
2021-07-21 00:19:31 -0500

    Added the begining of 5522A model

--- MetrologyLab/Fundamentals/Instruments.py
+++ MetrologyLab/Fundamentals/Instruments.py
@@ -1,12 +1,31 @@
-import xml.etree.ElementTree as ET
 import metrolopy as uc
+import functools
 
 
-class Instrument:
-    def __init__(self, path: str):
-        self.ORIGIN = path
-        self.ROOT = ET.parse(self.ORIGIN)
-        print(self.ROOT.find('INFORMATION').find("MFR").text)
+class Fluke_5522A:
+    def __init__(self):
+        self.MODEL = "5522A"
+        self.MFR = "FLUKE"
+        ## Fluke Generators
+        self.V_DC = InstFunciton(unit = uc.unit('V'),ranges = [
+            Interval(limits=[0, 329.9999E-3],
+                     nominal=lambda x : uc.gummy(x,unit = "mV",u = 20, uunit = "ppm"),
+                     error=lambda x: uc.gummy(0,unit = "mV",u = 0,uunit = "ppm"),
+                     resolution=0.0001,
+                     u_floor=uc.gummy(x=0, u=1, unit="mV", uunit="uV"),
+                     u_foor_stability=uc.gummy(x=0, u=1, unit="mV", uunit="uV"),
+                     u_resolution=uc.gummy(uc.UniformDist(center=0, half_width=0.0001 / 2), unit="mV", uunit="mV")),
+            Interval(limits=[0, 3.2999999E0],
+                     nominal=lambda x: uc.gummy(x, unit="V", u=11, uunit="ppm"),
+                     error=lambda x :uc.gummy(0, unit="V", u=0, uunit="ppm"),
+                     resolution=0.000001,
+                     u_floor=uc.gummy(x=0, u=1, unit="V", uunit="uV"),
+                     u_foor_stability=uc.gummy(x=0, u=1, unit="V", uunit="uV"),
+                     u_resolution=uc.gummy(uc.UniformDist(center=0, half_width=0.0001 / 2), unit="mV", uunit="mV"))
+        ])
+
+
+        ## DEFINITION OF INTERVALS
 
 
 class InstFunciton:
@@ -29,37 +48,37 @@ class InstFunciton:
 class Interval:
     """ An Interval is the fundamental configuration of any equipment.
     An equipment has always a nominal value that can be corrected given a calibration certificate,
-    they tend to have a minimum and maximum value of operation. A function can map the nominal value
-    to a real value with a certain uncertainty."""
+    the calibration certificate will give you the correct value that sometimes can be fitted into a curved
+    and is always associated to an uncertainty.
+    Instrument intervals tend to have a minimum and maximum value of operation.
+    A function can map the nominal value to a real value with a certain uncertainty.
 
-    def function(self):
+    """
+
+    def function(self, x=None):
         return x
 
-    def __init__(self,max,min = 0,nominal:function = lambda x:uc.gummy(x),error:function = lambda x:0,**kwargs):
+    def __init__(self,limits:list = [],
+                 nominal:function = lambda x:uc.gummy(x),
+                 error:function = lambda x:0,
+                 resolution = 0.1E-16,
+                 **kwargs):
         self.NOMINAL = nominal
         self.E = error
-        self.min = min
-        self.max = max
-        self.unit = self.NOMINAL(self.min).unit
-
-
+        self.min = min(limits)
+        self.max = max(limits)
+        self.unit = self.NOMINAL(0).unit
+        self.Res = resolution
+        self.u = []
         ## Uncertainties from different sources
-
-
+        for key in kwargs:
+            if "u_" in key:
+                self.u.append(kwargs[key])
 
     def __call__(self,x):
-        return self.NOMINAL(x)-self.E(x)
-
-
+        """Compute the uncertainty for a given value"""
+        return functools.reduce(lambda a,b:a+b,[self.NOMINAL(x),-self.E(x)]+self.u)
 
 
 if __name__ == "__main__":
-    I = Interval(max = 0.3299999,
-                 min = 0,
-                 nominal= lambda x:(uc.gummy(x,unit = 'mV',u = 15,uunit='ppm')),
-                 error = lambda x:uc.gummy(0.0,unit = 'mV',u= 0,uunit='ppm'))
-    I.style = "+-"
-
-    print(I(100))
-
-
+    I = Fluke_5522A()
